8ec9874990349ad1f6e2c9aaafb0e62d608ac1d6
Diego <diegojairhl@gmail.com>
2023-07-05 18:15:18 -0500

    chore: cleanup code

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
 // import './App.css';
-// import HighchartsComponent from './components/ParliamentGraph';
 import ParliamentChartD3 from './components/ParliamentChartD3';
 
 function App() {
   const data = [
-    { name: 'Progressive Party', color: 'blue', seats: 18 },
-    { name: 'Liberty Alliance', color: 'red', seats: 36 },
-    { name: 'Green Future', color: 'green', seats: 24 },
-    { name: 'Democratic Front', color: 'purple', seats: 25 },
-    { name: 'Unity Party', color: 'orange', seats: 20 },
-    { name: 'Conservative Coalition', color: 'black', seats: 15 },
+    {
+      party: 'Democratic Party',
+      seats: 52,
+      color: '#636cbb',
+    },
+    {
+      party: 'Republican Party',
+      seats: 39,
+      color: '#26d050',
+    },
+    {
+      party: 'Green Party',
+      seats: 67,
+      color: '#315cee',
+    },
+    {
+      party: 'Socialist Party',
+      seats: 26,
+      color: '#ff0000',
+    },
+    {
+      party: 'Libertarian Party',
+      seats: 23,
+      color: '#f8e71c',
+    },
+    {
+      party: 'Progressive Party',
+      seats: 19,
+      color: '#8c2a9a',
+    },
   ];
   return (
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        {/* <HighchartsComponent data={data} /> */}
         <ParliamentChartD3 data={data} />
       </div>
     </>
--- src/components/ParliamentChartD3.jsx
+++ src/components/ParliamentChartD3.jsx
@@ -1,47 +1,10 @@
+/* eslint-disable */
 import { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 import { parliamentChart } from 'd3-parliament-chart';
 import './parliamentChartD3.css';
-const inputData = [
-  {
-    party: 'Democratic Party',
-    seats: 52,
-    color: '#636cbb',
-    id: '2',
-  },
-  {
-    party: 'Republican Party',
-    seats: 39,
-    color: '#26d050',
-    id: '3',
-  },
-  {
-    party: 'Green Party',
-    seats: 67,
-    color: '#315cee',
-    id: '4',
-  },
-  {
-    party: 'Socialist Party',
-    seats: 26,
-    color: '#ff0000',
-    id: '5',
-  },
-  {
-    party: 'Libertarian Party',
-    seats: 23,
-    color: '#f8e71c',
-    id: '6',
-  },
-  {
-    party: 'Progressive Party',
-    seats: 19,
-    color: '#8c2a9a',
-    id: '7',
-  },
-];
 
-const ParliamentChart = () => {
+const ParliamentChart = ({ data: inputData, totalSeats }) => {
   const chartRef = useRef(null);
   const legendRef = useRef(null);
 
@@ -69,16 +32,16 @@ const ParliamentChart = () => {
     container.html(legendHtml);
   };
 
-  const addHoverInteraction = (chartContainer, legendContainer) => {
+  const addHoverInteraction = (chartContainer, legendContainer, data) => {
     const legendItems = legendContainer.selectAll('.legend-item');
 
     legendItems.on('mouseover', function () {
       const selectedColor = d3.select(this).attr('data-color');
-      const selectedId = inputData.find((item) => item.color === selectedColor).id;
+      const selectedId = data.find((item) => item.color === selectedColor).id;
 
       chartContainer.selectAll('circle').style('opacity', function () {
         const circleColor = d3.select(this).attr('fill');
-        const circleId = inputData.find((item) => item.color === circleColor).id;
+        const circleId = data.find((item) => item.color === circleColor).id;
 
         return circleColor === selectedColor && circleId === selectedId ? 1 : 0.1;
       });
@@ -89,16 +52,16 @@ const ParliamentChart = () => {
     });
   };
 
-  const addHoverInteractionOnCircles = (chartContainer) => {
+  const addHoverInteractionOnCircles = (chartContainer, data) => {
     const circles = chartContainer.selectAll('circle');
 
     circles.on('mouseover', function () {
       const selectedColor = d3.select(this).attr('fill');
-      const selectedId = inputData.find((item) => item.color === selectedColor).id;
+      const selectedId = data.find((item) => item.color === selectedColor).id;
 
       chartContainer.selectAll('circle').style('opacity', function () {
         const circleColor = d3.select(this).attr('fill');
-        const circleId = inputData.find((item) => item.color === circleColor).id;
+        const circleId = data.find((item) => item.color === circleColor).id;
 
         return circleColor === selectedColor && circleId === selectedId ? 1 : 0.1;
       });
@@ -109,16 +72,25 @@ const ParliamentChart = () => {
     });
   };
 
+  const assignUniqueIds = (parties) => {
+    return parties.map((party, index) => ({
+      ...party,
+      id: Math.random().toString(36) + index.toString(),
+    }));
+  };
+
   useEffect(() => {
     const chartContainer = d3.select(chartRef.current);
     const legendContainer = d3.select(legendRef.current);
 
-    const chart = createParliamentChart(chartContainer, inputData);
+    const data = assignUniqueIds(inputData);
+
+    const chart = createParliamentChart(chartContainer, data);
 
     appendChart(chartContainer, chart);
-    generateLegend(legendContainer, inputData);
-    addHoverInteraction(chartContainer, legendContainer);
-    addHoverInteractionOnCircles(chartContainer);
+    generateLegend(legendContainer, data);
+    addHoverInteraction(chartContainer, legendContainer, data);
+    addHoverInteractionOnCircles(chartContainer, data);
   }, []);
 
   return (
@@ -127,7 +99,7 @@ const ParliamentChart = () => {
         <svg id="pchart" ref={chartRef} width="100%" viewBox="0 0 800 500" />
 
         <div className="total-seats-ctn">
-          <spa className="total-seats-value">256</spa>
+          <span className="total-seats-value">256</span>
           <span className="total-seats-text">Bancas totales</span>
         </div>
       </div>
